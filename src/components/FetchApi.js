5300bd53f926a13dc26adc568007129cd9893390
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2021-10-01 22:03:26 +0900

    add: json

--- src/components/FetchApi.js
+++ src/components/FetchApi.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const FetchApi = () => {
-  return <div></div>;
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://my-json-server.typicode.com/yk-port/react-basic/tasks")
+      .then((res) => setTasks(res.data));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.id}: {task.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default FetchApi;
