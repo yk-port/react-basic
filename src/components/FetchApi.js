7a3040bb2fd4ba651054e8c8ff6abb868220e81f
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2021-10-01 22:23:20 +0900

    add: selected

--- src/components/FetchApi.js
+++ src/components/FetchApi.js
@@ -3,12 +3,22 @@ import axios from "axios";
 
 const FetchApi = () => {
   const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState({});
+  const [id, setId] = useState(1);
 
   useEffect(() => {
     axios
       .get("https://my-json-server.typicode.com/yk-port/react-basic/tasks")
       .then((res) => setTasks(res.data));
   }, []);
+
+  const getTask = () => {
+    axios
+      .get(
+        `https://my-json-server.typicode.com/yk-port/react-basic/tasks/${id}`
+      )
+      .then((res) => setSelectedTask(res.data));
+  };
 
   return (
     <div>
@@ -19,6 +29,15 @@ const FetchApi = () => {
           </li>
         ))}
       </ul>
+      <hr />
+      <label>set task id</label>
+      <br />
+      <input type="number" value={id} onChange={(e) => setId(e.target.value)} />
+      <br />
+      <button onClick={() => getTask()}>get task!</button>
+      <h4>
+        {selectedTask.id}: {selectedTask.title}
+      </h4>
     </div>
   );
 };
